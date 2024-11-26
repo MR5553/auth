02b35818eadd1e82a8f54d2b38af7bdd82f777be
Munna Kumar <shauryakumar42040er@gmail.com>
2024-11-26 18:58:17 +0530

    Fix: authLayout

--- client/src/pages/AuthLayout.tsx
+++ client/src/pages/AuthLayout.tsx
@@ -2,17 +2,17 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useAuthStore } from "../store/auth.store";
 
 export default function AuthLayout() {
-    // const { isAuthenticated, user } = useAuthStore((state) => state);
+    const { isAuthenticated, user } = useAuthStore((state) => state);
 
-    // if (!user?.is_verified && user?.profile_info?.email) {
-    //     return <Navigate to={`/verifyemail/${user._id}`} replace />;
-    // }
+    if (!user?.is_verified && user?.profile_info?.email) {
+        return <Navigate to={`/verifyemail/${user._id}`} replace />;
+    }
 
-    // if (!isAuthenticated && !user?.is_verified) {
-    //     return (
-    //         <Navigate to="/sign-up" replace />
-    //     )
-    // }
+    if (!isAuthenticated && !user?.is_verified) {
+        return (
+            <Navigate to="/sign-up" replace />
+        )
+    }
 
     return <Outlet />
 }
